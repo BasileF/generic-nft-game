6dedff06515e669494c266707d50cc3e58272581
Basile Fengos <basilefengos@Basiles-MacBook-Air.local>
2022-01-03 12:52:22 -0500

    Inherit openzeppelin contracts

--- contracts/GenericGame.sol
+++ contracts/GenericGame.sol
@@ -2,9 +2,43 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract GenericGame {
-  constructor() {
-    console.log("THIS IS MY GAME CONTRACT. NICE.");
+contract GenericGame is ERC721 {
+
+  struct CharacterAttributes {
+    string name;
+    string imageURI;
+    uint characterIndex;
+    uint hp;
+    uint maxHp;
+    uint attackDamage;
   }
+
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  CharacterAttributes[] defaultCharacters;
+
+  // We create a mapping from the nft's tokenId => that NFTs attributes.
+  mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+
+  // A mapping from an address => the NFTs tokenId. Gives me an ez way
+  // to store the owner of the NFT and reference it later.
+  mapping(address => uint256) public nftHolders;
+
+  constructor(string[] memory characterNames, string[] memory characterImageURIs, uint[] memory characterHp, uint[] memory characterAttackDmg) ERC721("Gods", "GODS"){
+    for (uint i = 0; i < characterNames.length; i++) {
+      defaultCharacters.push(CharacterAttributes(characterNames[i], characterImageURIs[i], i, characterHp[i], characterHp[i], characterAttackDmg[i]));
+
+      CharacterAttributes memory c = defaultCharacters[i];
+      console.log("Done initialiing %s w/ HP %s, img %s", c.name, c.hp, c.imageURI);
+    }
+    _tokenIds.increment();
+  }
+
+  
+
 }
--- scripts/run.js
+++ scripts/run.js
@@ -2,7 +2,16 @@ const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory(
     "GenericGame"
   );
-  const gameContract = await gameContractFactory.deploy();
+  const gameContract = await gameContractFactory.deploy(
+    ["Poseidon", "Zeus", "Ares"],
+    [
+      "https://www.kindpng.com/picc/m/249-2498164_almighty-reels-realm-of-poseidon-almighty-reels-realm.png",
+      "https://www.kindpng.com/picc/m/231-2315247_zeus-png-zeus-greek-god-zeus-transparent-png.png",
+      "https://www.kindpng.com/picc/m/50-505387_freetoedit-ares-gods-greek-mythology-god-of-war.png",
+    ],
+    [600, 420, 1000],
+    [300, 380, 260]
+  );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
 };
